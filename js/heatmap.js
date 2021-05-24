4f3723885b144e63d723279f86f77c6aeb7343e7
HopeNeely <hope.neely@gmail.com>
2021-05-24 09:02:08 -0400

    added heatmap.html code, js directory with heatmap js files, and css directory and files needed for heatmap.

---
+++ js/heatmap.js
@@ -0,0 +1,33 @@
+var myMap = L.map("map", {
+    center: [40.71, -73.94],
+    zoom: 11
+})
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap)
+
+//Function
+function handleSubmit() {
+    d3.event.preventDefault();
+}
+d3.json("static/data/mapdata.json", (dataFile) => {
+
+    var heatArray = [];
+
+    for (var i = 0; i < dataFile.length; i++) {
+        var lat = dataFile[i].LATITUDE
+        var lon = dataFile[i].LONGITUDE
+        heatArray.push([lat, lon])
+    }
+
+    var heat = L.heatLayer(heatArray, {
+        radius: 35,
+        blur: 15
+    }).addTo(myMap);
+})
