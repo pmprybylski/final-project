dd9d183139a2ec5ae2862467f699dbcac9b922c8
c-klee-run <kristy.allred@gmail.com>
2021-05-25 18:53:23 -0400

    updated index.html and heatmap.js files

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,7 +1,7 @@
 var myMap = L.map("map", {
     center: [40.71, -73.94],
     zoom: 11
-})
+});
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -10,7 +10,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-}).addTo(myMap)
+}).addTo(myMap);
 
 //Function
 function handleSubmit() {
@@ -30,4 +30,4 @@ d3.json("static/data/mapdata.json", (dataFile) => {
         radius: 35,
         blur: 15
     }).addTo(myMap);
-})
+});
