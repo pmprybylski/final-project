27d252cfde749461207c90160b33778e2593d318
HopeNeely <hope.neely@gmail.com>
2021-05-27 11:13:27 -0400

    Added new Mapbox api key that will allow url restrictions.

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiaG9wZW5lZWx5IiwiYSI6ImNrbjFzOXJoZzBwN2Iybm8yZWNnYjhhd3AifQ.rawox3f0mOXcbaxAtuSylw"
+const API_KEY = "pk.eyJ1IjoiaG9wZW5lZWx5IiwiYSI6ImNrcDcwem9pbjBidG8yd3FlYjR3dW95MTEifQ.RT9Peo9FoVwFkKtuJBeyDA"
